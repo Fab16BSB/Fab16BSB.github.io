3230e3195f8b4d319f30866bf225e541f33611a2
Fab.16 <fab_16@hotmail.fr>
2022-06-13 22:14:35 +0200

    Update info_page_publication.js

--- javascript/info_page_publication.js
+++ javascript/info_page_publication.js
@@ -8,7 +8,7 @@ const TAB = [
         state: "Publier",
         title: "Détection d'anomalie en temps réel dans un flux video",
         english: "Non disponible pour le moment",
-        french: "<a href=\"https://books.google.fr/books?hl=fr&lr=&id=xrFaEAAAQBAJ&oi=fnd&pg=PA437&dq=detection+d%27anomalie+dans+un+flux+video&ots=PBULCf0cv2&sig=FtPRJTMIC7_mZmS64tJgFLdBExE&redir_esc=y#v=onepage&q=detection%20d'anomalie%20dans%20un%20flux%20video&f=false\"> Version officiel </a>"
+        french: "<a href=\"https://editions-rnti.fr/render_pdf.php?p=1002757\"> Version officiel </a>"
       },
     ]
   },
