a178cac0ea555fc972f64f0ed4dd33bf0c58de0a
Fab.16 <fab_16@hotmail.fr>
2022-06-14 18:21:19 +0200

    Update info_page_enseignement.js

--- javascript/info_page_enseignement.js
+++ javascript/info_page_enseignement.js
@@ -4,24 +4,24 @@ const TAB = [
     info: [
       {
         year: "2021-2022",
-        theme: "Programation Orienté Objet en Java",
+        theme: "Programmation Orienté Objet en Java",
         lvl: "Licence 2",
         place: "À distance",
         role: "Chargé de TP"
       },
       {
         year: "2020-2021",
-        theme: "Programation Orienté Objet en Java",
+        theme: "Programmation Orienté Objet en Java",
         lvl: "Licence 2",
         place: "À distance",
         role: "Chargé de TP"
       },
       {
         year: "2019-2020",
-        theme: "Programation Orienté Objet en Java",
+        theme: "Programmation Orienté Objet en Java",
         lvl: "Licence 2",
         place: "Présentiel",
-        role: "Assitant"
+        role: "Assistant"
       },
     ]
   },
