267289314065559efbad685f88c3ed513236ff6d
Fab.16 <fab_16@hotmail.fr>
2022-06-14 18:19:23 +0200

    Update info_page_experience.js

--- javascript/info_page_experience.js
+++ javascript/info_page_experience.js
@@ -11,7 +11,7 @@ const TAB = [
       {
         year: "2019",
         contact: "Stage",
-        subject: "Développement d’un modèle deep learning en python à l’aide Keras dans le but détecter en temps réel des d’objets et d’anomalies sur des données vidéos.",
+        subject: "Développement d’un modèle deep learning en python à l’aide Keras dans le but de détecter en temps réel des d’objets et d’anomalies sur des données vidéos.",
       },
       {
         year: "2018",
@@ -21,12 +21,12 @@ const TAB = [
       {
         year: "2017",
         contact: "Stage",
-        subject: "Développement d’un outil de crawling web en java utilisant la bibliothèque HtmlUnit dans le but de recupérer des petites annonces sur des site immobilier / automobile mexicain.",
+        subject: "Développement d’un outil de crawling web en Java utilisant la bibliothèque HtmlUnit dans le but de récupérer des petites annonces sur des site immobilier / automobile mexicain.",
       },
       {
         year: "2016",
         contact: "Stage",
-        subject: "Développement d’une application IOS en Apcelerator Titanium appelée \"Soldout\" disponible sur l’App Store permetant de consulter, réserver et acheter des places de spectacles.",
+        subject: "Développement d’une application IOS en Apcelerator Titanium appelée \"Soldout\" disponible sur l’App Store permettant de consulter, réserver et acheter des places de spectacles.",
       },
     ]
   },
@@ -37,23 +37,23 @@ const TAB = [
     info: [
       {
         year: "2018 - 2019",
-        subject: "Développement d’une version C et java du réseau de neurones SOM utilisant iris.data comme jeux de données.<br><br>Développement d’un CNN en python à l’aide de la bibliothèque KERAS pour la reconnaissance d’image.",
+        subject: "Développement d’une version C et Java du réseau de neurones SOM utilisant iris.data comme jeux de données.<br><br>Développement d’un CNN en python à l’aide de la bibliothèque KERAS pour la reconnaissance d’image.",
       },
       {
         year: "2017",
-        subject: "Développement d’une application android permettant la gestion d’un emploi du temps.",
+        subject: "Développement d’une application Android permettant la gestion d’un emploi du temps.",
       },
       {
         year: "2016",
-        subject: "Développement d’un jeux vidéo d’infiltration en c# avec Unity.<br><br>Développement d’un jeu de stratégie en Java avec bibliothèque Swing inspiré du jeu Plague Inc.",
+        subject: "Développement d’un jeu vidéo d’infiltration en C# avec Unity.<br><br>Développement d’un jeu de stratégie en Java avec bibliothèque Swing inspiré du jeu Plague Inc.",
       },
       {
         year: "2015",
-        subject: "Création de site web avec HTML,CSS,PHP, architecture MVC et base de données PotsgreSQL.",
+        subject: "Création de site web avec HTML, CSS, PHP, architecture MVC et base de données PotsgreSQL.",
       },
       {
         year: "2014",
-        subject: "Projet de Spécialisation: Bac S option ISN [Informatique et Science du Numérique].<br>Réalisation d’un générateur de mot de passe aléatoire en Python.",
+        subject: "Projet de Spécialisation : Bac S option ISN [Informatique et Science du Numérique].<br>Réalisation d’un générateur de mot de passe aléatoire en Python.",
       },
     ]
   },
