037b76a6f6aec12536109c19551b5abb526d48ba
Fab.16 <fab_16@hotmail.fr>
2022-06-19 03:35:37 +0200

    Update info_page_experience.js

--- javascript/info_page_experience.js
+++ javascript/info_page_experience.js
@@ -95,7 +95,7 @@ const TAB = [
       },
 
       {
-                name: "Academical Projects",
+        name: "Academical Projects",
         label: ["Years", "Subjets"],
         info: [
           {
