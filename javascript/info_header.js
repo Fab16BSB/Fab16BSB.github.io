b5aaa1155ad4ab437611f81e2a418aaf52b7b933
Fab.16 <fab_16@hotmail.fr>
2022-11-06 13:26:07 +0100

    Update info_header.js
    add new page name (competence / skill / competencia)

--- javascript/info_header.js
+++ javascript/info_header.js
@@ -4,20 +4,20 @@ const HeaderInfo = {
 		status: "Doctorant 3ème année",
 		lieu: "France",
 		interet: "Intérêts : Jeux vidéo, Programmation, Série, Manga, Animé",
-		menu: ["Formations", "Expériences", "Publications", "Récompenses", "Enseignements"],
+		menu: ["Formations", "Expériences", "Compétences", "Publications", "Récompenses", "Enseignements"],
 	},
 
 	english: {
 		status: "3rd year PhD student",
 		lieu: "France",
 		interet: "Interests: Video games, Programming, Series, Manga, Anime",
-		menu: ["Studies", "Experiences", "Publications", "Awards", "Teachings"],
+		menu: ["Studies", "Experiences", "Skills", "Publications", "Awards", "Teachings"],
 	},
 
 	spanish: {
 		status: "Doctorado de 3er año",
 		lieu: "Francia",
 		interet: "Intereses: Videojuegos, Programación, Series, Manga, Anime",
-		menu: ["Estudios", "Experiencias", "Publicaciones", "Premios", "Enseñanzas"],
+		menu: ["Estudios", "Experiencias", "Competencias", "Publicaciones", "Premios", "Enseñanzas"],
 	},
 }
