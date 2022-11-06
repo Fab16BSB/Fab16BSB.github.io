c5bcd12475e6d79bef8fb125bbefa6993cafee5f
Fab.16 <fab_16@hotmail.fr>
2022-11-06 13:27:59 +0100

    Update info_footer.js

--- javascript/info_footer.js
+++ javascript/info_footer.js
@@ -2,16 +2,16 @@ const FooterInfo = {
 	
 	french: {
 		tableau: ["Mes Réseaux", "Ma Communauté"],
-		texte: `Icônes provenant de <a href="http://www.flaticon.com" title="www.flaticon.com"> flaticon </a><br>Dernière mise à jour le 21/06/2022<br>Site créé le 02/06/2022`
+		texte: `Icônes provenant de <a href="http://www.flaticon.com" title="www.flaticon.com"> flaticon </a><br>Dernière mise à jour le 06/11/2022<br>Site créé le 02/06/2022`
 	},
 
 	spanish: {
 		tableau: ["Mis redes", "Mi comunidad"],
-		texte: `Iconos de <a href="http://www.flaticon.com" title="www.flaticon.com"> flaticon </a><br>Última actualización el 21/06/2022<br>Página creada el 02/06/2022`
+		texte: `Iconos de <a href="http://www.flaticon.com" title="www.flaticon.com"> flaticon </a><br>Última actualización el 06/11/2022<br>Página creada el 02/06/2022`
 	},
 
 	english: {
 		tableau: ["My Networks", "My Community"],
-		texte: `Icons from <a href="http://www.flaticon.com" title="www.flaticon.com"> flaticon </a><br>Last update 21/06/2022<br>Website created 02/06/2022`
+		texte: `Icons from <a href="http://www.flaticon.com" title="www.flaticon.com"> flaticon </a><br>Last update 06/11/2022<br>Website created 02/06/2022`
 	},
 }
