7f28c5ca64c686ffab8ad3da8ba513834fa25166
Fab.16 <fab_16@hotmail.fr>
2022-11-06 13:24:05 +0100

    Add files via upload

---
+++ javascript/info_page_competence.js
@@ -0,0 +1,91 @@
+const TAB = [
+  {
+    french: [
+      {
+        name: "",
+        label: ["Langages"],
+        info: [
+          {
+            skill: `
+            <span class='row-icons-container'> 
+              <div class='skill-container'> 
+               <img src='image/skill/html.png' alt='html5' class='skill-icon bordered b-good' /> 
+               <span class='skill-text sk-good'>GOOD</span> 
+              </div> 
+              <div class='skill-container'> 
+                <img src='image/skill/css.png' alt='css' class='skill-icon bordered b-good' />
+                <span class='skill-text sk-good'>GOOD</span>
+              </div> 
+              <div class='skill-container'> 
+                <img src='image/skill/js.png' alt='javascript' class='skill-icon bordered b-med' />
+                <span class='skill-text sk-med'>MEDIUM</span>
+              </div> 
+              <div class='skill-container'>
+                <img src='image/skill/sql.png' alt='sql' class='skill-icon bordered b-med' />
+                <span class='skill-text sk-med'>MEDIUM</span>
+              </div> 
+              <div class='skill-container'>
+                <img src='image/skill/c.png' alt='langage C' class='skill-icon bordered b-med' />
+                <span class='skill-text sk-med'>MEDIUM</span>
+              </div>
+              <div class='skill-container'>
+                <img src='image/skill/java.png' alt='java' class='skill-icon bordered b-good' />
+                <span class='skill-text sk-good'>GOOD</span>
+              </div>
+              <div class='skill-container'>
+                <img src='image/skill/python.png' alt='python' class='skill-icon bordered b-med' />
+                <span class='skill-text sk-med'>MEDIUM</span>
+              </div>
+            </span>`
+          }
+        ],
+      },
+
+      {
+        name: "",
+        label: ["Framworks"],
+        info: [
+          {
+            framwork: ` <span class='row-icons-container'> 
+              <div class='skill-container'>
+                <img src='image/skill/sklearn.png' alt='sk-learn' class='skill-icon bordered b-med' /> 
+                <span class='skill-text sk-med'>MEDIUM</span> 
+              </div> 
+              <div class='skill-container'>
+                <img src='image/skill/keras.png' alt='keras' class='skill-icon bordered b-good' />
+                <span class='skill-text sk-good'>GOOD</span>  
+              </div> 
+              <div class='skill-container'> 
+                <img src='image/skill/tensorflow.png' alt='tensorflow' class='skill-icon bordered b-low' />
+                <span class='skill-text sk-low'>LOW</span> 
+              </div> 
+              <div class='skill-container'>
+                <img src='image/skill/pytorch.png' alt='pytorch' class='skill-icon bordered b-low' /> 
+                <span class='skill-text sk-low'>LOW</span> 
+              </div>
+            </span>`
+          }
+        ],
+      },
+
+      {
+        name: "",
+        label: ["IDE"],
+        info: [
+          {
+            ide: `<span class='row-icons-container'>
+              <div class='skill-container'>
+                <img src='image/skill/eclipse.png' alt='eclipse' class='skill-icon bordered b-good'>
+                <span class='skill-text sk-good'>GOOD</span> 
+              </div>
+              <div class='skill-container'>
+                <img src='image/skill/jupyter.png' alt='jupyter notebook' class='skill-icon bordered b-good'>
+                <span class='skill-text sk-good'>GOOD</span> 
+              </div>
+            </span>`
+          }
+        ],
+      },
+    ],
+  }
+]
