6b64826f104f04187766c0677a7e21c044002043
Fab.16 <fab_16@hotmail.fr>
2023-01-06 01:22:14 +0100

    Update info_page_enseignement.js

--- javascript/info_page_enseignement.js
+++ javascript/info_page_enseignement.js
@@ -2,27 +2,46 @@ const TAB = [
   {
     french :[
       {
-        label: ["Années", "Thème", "Niveau", "Lieux", "Poste"],
+        label: ["Années", "Thème", "Niveau", "Lieux", "Volume", "Poste"],
         info: [
+          {
+            year: "2022-2023",
+            theme: "Deep learning",
+            lvl: "Master 1",
+            place: "Présentiel",
+            quantity: "30h",
+            role: "Chargé de cours"
+          },
+          {
+            year: "2022-2023",
+            theme: "Programmation Orienté Objet en Java",
+            lvl: "Licence 2",
+            place: "À distance",
+            quantity: "50h",
+            role: "Chargé de cours"
+          },
           {
             year: "2021-2022",
             theme: "Programmation Orienté Objet en Java",
             lvl: "Licence 2",
             place: "À distance",
-            role: "Chargé de TP"
+            quantity: "50h",
+            role: "Chargé de cours"
           },
           {
             year: "2020-2021",
             theme: "Programmation Orienté Objet en Java",
             lvl: "Licence 2",
             place: "À distance",
-            role: "Chargé de TP"
+            quantity: "35h",
+            role: "Chargé de cours"
           },
           {
             year: "2019-2020",
             theme: "Programmation Orienté Objet en Java",
             lvl: "Licence 2",
             place: "Présentiel",
+            quantity: "15h",
             role: "Assistant"
           },
         ]
@@ -31,13 +50,30 @@ const TAB = [
 
     english :[
       {
-        label: ["Years", "Theme", "Level", "Place", "Role"],
+        label: ["Years", "Theme", "Level", "Place", "Volume", "Role"],
         info: [
+           {
+            year: "2022-2023",
+            theme: "Deep learning",
+            lvl: "Master 1",
+            place: "Face to face",
+            quantity: "30h",
+            role: "Work supervisor"
+          },
+          {
+            year: "2022-2023",
+            theme: "Oriented Object Programming in Java",
+            lvl: "Licence 2",
+            place: "Distance",
+            quantity: "50h",
+            role: "Work supervisor"
+          },
           {
             year: "2021-2022",
             theme: "Oriented Object Programming in Java",
             lvl: "License 2",
             place: "Distance",
+            quantity: "50h",
             role: "Work supervisor"
           },
           {
@@ -45,6 +81,7 @@ const TAB = [
             theme: "Oriented Object Programming in Java",
             lvl: "License 2",
             place: "Distance",
+            quantity: "35h",
             role: "Work supervisor"
           },
           {
@@ -52,6 +89,7 @@ const TAB = [
             theme: "Oriented Object Programming in Java",
             lvl: "License 2",
             place: "Face to face",
+            quantity: "15h",
             role: "Assistant"
           },
         ]
@@ -60,27 +98,46 @@ const TAB = [
 
     spanish :[
       {
-        label: ["Años", "Tema", "Nivel", "Lugar", "Rol"],
+        label: ["Años", "Tema", "Nivel", "Lugar", "Volumen", "Rol"],
         info: [
+           {
+            year: "2022-2023",
+            theme: "Deep Learning",
+            lvl: "Master 1",
+            place: "Presencial",
+            quantity: "30h",
+            role: "Responsable del curso"
+          },
+           {
+            year: "2022-2023",
+            theme: "Programación Orientada a Objetos con Java",
+            lvl: "Licencia 2",
+            place: "A distancia",
+            quantity: "50h",
+            role: "Responsable del curso"
+          },
           {
             year: "2021-2022",
             theme: "Programación Orientada a Objetos con Java",
             lvl: "Licencia 2",
             place: "A distancia",
-            role: "Director de trabajos dirigidos"
+            quantity: "50h",
+            role: "Responsable del curso"
           },
           {
             year: "2020-2021",
             theme: "Programación Orientada a Objetos con Java",
             lvl: "Licencia 2",
             place: "A distancia",
-            role: "Director de trabajos dirigidos"
+            quantity: "35h",
+            role: "Responsable del curso"
           },
           {
             year: "2019-2020",
             theme: "Programación Orientada a Objetos con Java",
             lvl: "Licencia 2",
             place: "Presencial",
+            quantity: "15h",
             role: "Ayudante"
           },
         ]
